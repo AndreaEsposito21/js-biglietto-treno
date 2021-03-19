d78c9970f09cec0885ceac3fff108b935ff39850
Andrea Esposito <andexpox@gmail.com>
2021-03-19 17:17:32 +0100

    end

--- js/script.js
+++ js/script.js
@@ -15,14 +15,33 @@ console.log(anniPasseggero);
 // Parseint con numeri interi
 var numeroKmPercorrenzaNumb = parseInt(numeroKmPercorrenza);
 var anniPasseggeroNumb = parseInt(anniPasseggero);
-console.log(numeroKmPercorrenzaNumb);
-console.log(anniPasseggeroNumb);
+console.log('numero km percorsi', numeroKmPercorrenzaNumb);
+console.log('anni passeggero', anniPasseggeroNumb);
 
 // Prezzo biglietto
-var prezzoBiglietto = parseInt(numeroKmPercorrenzaNumb * 0.21);
-console.log(prezzoBiglietto);
+var prezzoBiglietto = numeroKmPercorrenzaNumb * 0.21;
+console.log('prezzo biglietto', prezzoBiglietto);
 
+// Prezzo Scontato
 var prezzoScontatoOver65 = prezzoBiglietto - (prezzoBiglietto * 0.4);
 var prezzoScontatoUnder18 = prezzoBiglietto - (prezzoBiglietto * 0.2);
-console.log(prezzoScontatoOver65);
-console.log(prezzoScontatoUnder18);
+console.log('prezzo over 65', prezzoScontatoOver65);
+console.log('prezzo under 18', prezzoScontatoUnder18);
+
+
+// Comparazione prezzi
+var messaggioFinale;
+
+if (anniPasseggeroNumb <= 18) {
+    messaggioFinale = prezzoScontatoUnder18;
+} else if (anniPasseggeroNumb >= 65) {
+    messaggioFinale = prezzoScontatoOver65;
+} else {
+    messaggioFinale = prezzoBiglietto;
+}
+console.log('messaggio finale', messaggioFinale);
+
+// Prezzo Finale
+var prezzoFinale = messaggioFinale.toFixed(2);
+alert(prezzoFinale);
+// document.getElementById("price").innerHTML = prezzoFinale;
